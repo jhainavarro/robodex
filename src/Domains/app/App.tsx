14f48dcafcb734762c2ccb6c0aa0459fdac961ab
Jhai Navarro <jnavarro@tauria.com>
2022-03-17 01:13:37 +0800

    Initial Robodex branding

---
+++ src/Domains/app/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "Shared/icons/logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Robodex</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
