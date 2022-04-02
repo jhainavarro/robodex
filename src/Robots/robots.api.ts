188e0fe59be38d7b266f1051bcd04046cc5eaca5
Jhai Navarro <jnavarro@tauria.com>
2022-04-02 14:55:15 +0800

    Rename function defs inside API hooks

--- src/Robots/robots.api.ts
+++ src/Robots/robots.api.ts
@@ -15,10 +15,10 @@ const ROBOTS_API = env.get("REACT_APP_ROBOTS_API").required().asString();
  * Gets the list of robots from the Robodex
  */
 export function useRobots(): UseQueryResult<Robot[]> {
-  const getRobots = async () => {
+  async function getRobots() {
     const response = await fetch(`${ROBOTS_API}/robots`);
-    return (await response.json()) as Robot[]; // FIXME: Typing
-  };
+    return (await response.json()) as Robot[];
+  }
 
   return useQuery(ROBOTS_KEY, () => getRobots());
 }
@@ -27,10 +27,10 @@ export function useRobots(): UseQueryResult<Robot[]> {
  * Gets a single robot from the Robodex
  */
 export function useRobot(guid: string = ""): UseQueryResult<Robot | undefined> {
-  const getRobot = async (guid: string) => {
+  async function getRobot(guid: string) {
     const response = await fetch(`${ROBOTS_API}/robots/${guid}`);
-    return (await response.json()) as Robot[]; // FIXME: Typing
-  };
+    return (await response.json()) as Robot[];
+  }
 
   return useQuery([ROBOTS_KEY, guid], () => getRobot(guid));
 }
@@ -39,28 +39,28 @@ export function useRobot(guid: string = ""): UseQueryResult<Robot | undefined> {
  * Creates or edits a robot in the Robodex
  */
 export function useSaveRobot() {
-  const add = async (robot: AddRobotInput) => {
+  async function addRobot(robot: AddRobotInput) {
     const response = await fetch(`${ROBOTS_API}/robots`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(robot),
     });
 
-    return (await response.json()) as Robot; // FIXME: Typing
-  };
+    return (await response.json()) as Robot;
+  }
 
-  const edit = async (robot: EditRobotInput) => {
+  async function editRobot(robot: EditRobotInput) {
     const response = await fetch(`${ROBOTS_API}/robots/${robot.guid}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(robot),
     });
 
-    return (await response.json()) as Robot; // FIXME: Typing
-  };
+    return (await response.json()) as Robot;
+  }
 
   return useMutation((robot: AddRobotInput | EditRobotInput) =>
-    "guid" in robot ? edit(robot) : add(robot)
+    "guid" in robot ? editRobot(robot) : addRobot(robot)
   );
 }
 
@@ -70,13 +70,13 @@ export function useSaveRobot() {
 export function useDeleteRobot() {
   const queryClient = useQueryClient();
 
-  const remove = (guid: string) => {
+  function deleteRobot(guid: string) {
     return fetch(`${ROBOTS_API}/robots/${guid}`, {
       method: "DELETE",
     });
-  };
+  }
 
-  return useMutation((guid: string) => remove(guid), {
+  return useMutation((guid: string) => deleteRobot(guid), {
     onSuccess(_, guid) {
       queryClient.removeQueries([ROBOTS_KEY, guid]);
       queryClient.setQueryData(ROBOTS_KEY, (list: Robot[] = []) =>
